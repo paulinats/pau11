e756ffa50cf18453a443df4b90a48bd5cb52eb7f
paulinats <71715041+paulinats@users.noreply.github.com>
2021-02-07 15:51:55 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./styles.css";
 
 export default function App() {
   return (
-    <div className="App"><div>
+    <div className="App">
+      <div></div>
       <h1>Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
