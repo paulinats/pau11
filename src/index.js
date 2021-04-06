1225928305c238c1f257f2839ce881948bd43caf
paulinats <71715041+paulinats@users.noreply.github.com>
2021-04-06 20:52:41 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom";
 
 import App from "./App";
 kjhkjhk
-const rootEsdfment = pau
-ReactDOM.ren
+const rootEsdfment = pauasdd
+ReactDOM.rensdfsdf
   <StrictMode>
     <App />
   </StrictMode>,<div>
